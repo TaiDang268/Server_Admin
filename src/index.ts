f5869f25b771f14413469b5f8f2cd9d82882fb54
Đặng Tài <taidang268@gmail.com>
2023-10-06 11:53:44 +0700

    feat: creat schemas author

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-const name: string = 'Dư Thanh '
-console.log(name)
+import express from 'express'
+import { run } from './services/database.service'
+import { connectDB, createIndexAuthor } from './services/connectDB'
+const app = express()
+const router = express.Router()
+const PORT = 5555
+
+app.use((req, res, next) => {
+  console.log('sdsd')
+  next()
+})
+// run().catch(console.dir)
+connectDB().then(() => {
+  createIndexAuthor()
+})
+router.get('/tweets', (req, res) => {
+  res.json({ data: 'asd' })
+})
+app.use('/api', router)
+app.listen(PORT, () => {
+  console.log(`Listen on port ${PORT}`)
+})
